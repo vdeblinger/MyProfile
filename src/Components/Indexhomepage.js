9b24a70cf720d8a73aa118f2e314140d3bf46bef
Tori Deblinger <vdeblinger@yahoo.com>
2023-01-13 15:03:30 -0600

    third commit

--- src/Components/Indexhomepage.js
+++ src/Components/Indexhomepage.js
@@ -7,9 +7,8 @@ const IndexForHomePage = () => {
         <div id="homepage">
             <h1>Tori Deblinger</h1>
             <div id="titles">
-                <h2>Full Stack Web Developer</h2>
-                <h2>UI/UX Developer</h2>
-                <h2>Frontend/Backend Engineer</h2>
+                <h2>Full Stack Web Developer/Software Engineer</h2>
+                
             </div>
             {/* <div>
                 <a><img src="https://www.facebook.com/photo.php?fbid=10215598184560800&set=pb.1026261510.-2207520000.&type=3" alt="Photo of Tori Deblinger Sitting on a porch" /></a>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Projects = () => {
     return (
-        <div>
+        <div id="projects">
             <h2>Projects</h2>
+            <a href="https://thesweetspot.netlify.app/" id="project-link">The Sweet Spot</a>
+            <br />
+            <a href="https://vermillion-elf-79bd6f.netlify.app/" id="project-link">The Garage Sale</a>
         </div>
     )
 }
